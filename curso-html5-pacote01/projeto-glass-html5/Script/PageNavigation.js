0efd4e374461e48c7ae162d64823336d66b331e4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-10-16 22:51:42 -0300

    change images

--- curso-html5-pacote01/projeto-glass-html5/Script/PageNavigation.js
+++ curso-html5-pacote01/projeto-glass-html5/Script/PageNavigation.js
@@ -4,3 +4,10 @@ function changeIcon(icon){
 
     image.src = icon
 }
+
+function setDefaultIcon(){
+
+    image = document.getElementById("icon");
+
+    image.src = "/curso-html5-pacote01/projeto-glass-html5/View/Images/glass-oculos-preto-peq.png"
+}
